25af3d2645b375b3ed4272470dcfbd10116172d2
Janus Jacob Rønne <roenne@gmail.com>
2022-10-16 20:10:23 +0200

    16102022

--- starter/09-Omnifood-Optimizations/js/script.js
+++ starter/09-Omnifood-Optimizations/js/script.js
@@ -1,20 +1,34 @@
+console.log('Hello world!');
+
+const myName = 'Janus Rønne';
+const h1 = document.querySelector('.heading-primary');
+
+console.log(myName);
+console.log(h1);
+
+h1.addEventListener('click', function () {
+  h1.textContent = myName;
+  h1.style.backgroundColor = 'red';
+  h1.style.padding = '5rem';
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
 
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
 
